55634fa63b76020c4e403faa9fc7763f35986a2b
smarttamildotcom <advocatebala.2010@gmail.com>
2026-09-17 11:34:37 +0800

    Assemble fruit and animal jigsaw artwork

---
+++ src/lib/assets/fruit-animal-jigsaw.ts
@@ -0,0 +1,19 @@
+import { FRUIT_ANIMAL_JIGSAW_PART_0 } from "./fruit-animal-jigsaw-0";
+import { FRUIT_ANIMAL_JIGSAW_PART_1 } from "./fruit-animal-jigsaw-1";
+import { FRUIT_ANIMAL_JIGSAW_PART_2 } from "./fruit-animal-jigsaw-2";
+import { FRUIT_ANIMAL_JIGSAW_PART_3 } from "./fruit-animal-jigsaw-3";
+import { FRUIT_ANIMAL_JIGSAW_PART_4 } from "./fruit-animal-jigsaw-4";
+import { FRUIT_ANIMAL_JIGSAW_PART_5 } from "./fruit-animal-jigsaw-5";
+import { FRUIT_ANIMAL_JIGSAW_PART_6 } from "./fruit-animal-jigsaw-6";
+import { FRUIT_ANIMAL_JIGSAW_PART_7 } from "./fruit-animal-jigsaw-7";
+import { FRUIT_ANIMAL_JIGSAW_PART_8 } from "./fruit-animal-jigsaw-8";
+
+export const FRUIT_ANIMAL_JIGSAW_PNG = FRUIT_ANIMAL_JIGSAW_PART_0 +
+  FRUIT_ANIMAL_JIGSAW_PART_1 +
+  FRUIT_ANIMAL_JIGSAW_PART_2 +
+  FRUIT_ANIMAL_JIGSAW_PART_3 +
+  FRUIT_ANIMAL_JIGSAW_PART_4 +
+  FRUIT_ANIMAL_JIGSAW_PART_5 +
+  FRUIT_ANIMAL_JIGSAW_PART_6 +
+  FRUIT_ANIMAL_JIGSAW_PART_7 +
+  FRUIT_ANIMAL_JIGSAW_PART_8;
