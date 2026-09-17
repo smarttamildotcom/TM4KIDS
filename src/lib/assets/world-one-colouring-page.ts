213024889feeaf3ce19ee6b11ca349e2542661e7
smarttamildotcom <advocatebala.2010@gmail.com>
2026-09-17 10:39:47 +0800

    Join supplied Questy colouring page asset

---
+++ src/lib/assets/world-one-colouring-page.ts
@@ -0,0 +1,13 @@
+import { WORLD_ONE_COLOURING_PAGE_0 } from "./world-one-colouring-page-0";
+import { WORLD_ONE_COLOURING_PAGE_1 } from "./world-one-colouring-page-1";
+import { WORLD_ONE_COLOURING_PAGE_2 } from "./world-one-colouring-page-2";
+import { WORLD_ONE_COLOURING_PAGE_3 } from "./world-one-colouring-page-3";
+import { WORLD_ONE_COLOURING_PAGE_4 } from "./world-one-colouring-page-4";
+import { WORLD_ONE_COLOURING_PAGE_5 } from "./world-one-colouring-page-5";
+import { WORLD_ONE_COLOURING_PAGE_6 } from "./world-one-colouring-page-6";
+import { WORLD_ONE_COLOURING_PAGE_7 } from "./world-one-colouring-page-7";
+import { WORLD_ONE_COLOURING_PAGE_8 } from "./world-one-colouring-page-8";
+import { WORLD_ONE_COLOURING_PAGE_9 } from "./world-one-colouring-page-9";
+import { WORLD_ONE_COLOURING_PAGE_10 } from "./world-one-colouring-page-10";
+
+export const WORLD_ONE_COLOURING_PAGE_PNG = WORLD_ONE_COLOURING_PAGE_0 + WORLD_ONE_COLOURING_PAGE_1 + WORLD_ONE_COLOURING_PAGE_2 + WORLD_ONE_COLOURING_PAGE_3 + WORLD_ONE_COLOURING_PAGE_4 + WORLD_ONE_COLOURING_PAGE_5 + WORLD_ONE_COLOURING_PAGE_6 + WORLD_ONE_COLOURING_PAGE_7 + WORLD_ONE_COLOURING_PAGE_8 + WORLD_ONE_COLOURING_PAGE_9 + WORLD_ONE_COLOURING_PAGE_10;
